6462f9b5350900125aa4a6390436f4d3d6a7bdfa
Handrew <andrew.a.han@gmail.com>
2018-08-20 22:53:23 -0400

    create another component to display the random Numbers

---
+++ mathgames/components/RandomNumberGen/RandNumDisplayer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default RandNumDisplayer = props => {
+    return (
+        <View>
+            
+        </View>
+    )
+}
--- mathgames/components/RandomNumberGen/RandomNumberGen.js
+++ mathgames/components/RandomNumberGen/RandomNumberGen.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import RandNumDisplayer from './RandNumDisplayer';
 
 export default class RandomNumberGen extends React.Component {
-    state = {
-        firstNum: []
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+        // with es6 classes, React does not autobind functions inside components in this whole class. So you have to manually bind 'this' so that you can call equationGenerator method/function anywhere.
+        this.equationGenerator = this.equationGenerator.bind(this);
+    }
+
+    equationGenerator () {
+        let first, operator = ['+', '-', '*', '/'], second;
+
+        first = Math.floor(Math.random()* 100);
+        let randOperator = operator[Math.floor(Math.random() * operator.length)]
+        second = Math.floor(Math.random()* 100);
+
+        // each case should use a component to display the equations on to the screen
+        switch ( randOperator ) {
+            case '+':
+                <RandNumDisplayer />
+            break;
+            default:
+                <Text>Hello</Text>
+        }
+
     }
 
     render() {
