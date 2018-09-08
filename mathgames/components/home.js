3a058e41f62a9caba21ad7c39ea79e832e52f1f8
Handrew <andrew.a.han@gmail.com>
2018-09-08 19:23:43 -0400

    create home.js

---
+++ mathgames/components/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
+
+export default class Home extends React.Component {
+    render () {
+        return (
+            <View>
+
+            </View>
+        )
+    }
+}
