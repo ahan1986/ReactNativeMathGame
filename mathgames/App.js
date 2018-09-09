d2b405e1e776ba7365ca9161b49f1fa164799f47
Handrew <andrew.a.han@gmail.com>
2018-09-09 00:51:56 -0400

    start with homepage and then have button to move to the game

--- mathgames/App.js
+++ mathgames/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
 import Game from './components/game';
+import Home from './components/home';
 
 export default class App extends React.Component {
+  state = {
+    homepage: 'home'
+  }
+
+  pageChanger = () => {
+    this.state.homepage = 'Game';
+    this.setState({
+      homepage: this.state.homepage
+    });
+  }
+
+  currentPage = (event) => {
+    if(this.state.homepage == 'home') {
+      return <Home pageChanger={this.pageChanger} />
+    } else if(this.state.homepage == 'Game') {
+      return <Game />
+    }
+  }
+
   render() {
     return (
       <View style={{flex: 1}} >
-        <Game />
+        {this.currentPage(this.state.homepage)}
       </View>
     )
   }
-}
+};
--- mathgames/components/home.js
+++ mathgames/components/home.js
@@ -2,11 +2,28 @@ import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
 
 export default class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.startTheGame.bind(this);
+    }
+
+    startTheGame = () => {
+        this.props.pageChanger()
+    }
+
     render () {
         return (
-            <View>
-
+            <View style={styles.container} >
+                <Button title='Start' onPress={this.startTheGame} />
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
